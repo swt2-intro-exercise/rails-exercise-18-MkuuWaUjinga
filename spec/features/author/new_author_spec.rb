c3438534d5c095e5bd8da6f8698b2abafb7ce529
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2018-11-07 00:11:24 +0100

    Failing test for #4

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -9,10 +9,18 @@ describe "New author page", type: :feature do
   it "should have text inputs for an author's first name, last name, and homepage" do
     visit new_author_path
 
-    # these are the standard names given to inputs by the form builder
     expect(page).to have_field('author[first_name]')
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
+  end
+
+  it "should save filled in data" do
+    visit new_author_path
+
+    page.fill_in 'author[first_name]', with: 'Alan'
+    page.fill_in 'author[first_name]', with: 'Turing'
+    page.fill_in 'author[first_name]', with: 'http://wikipedia.de/Alan_Turing'
+    find('input[type="submit"]').click
   end
 
 end
