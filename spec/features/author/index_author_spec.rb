bc4ac1bfdbdba334c99ae04b430991228fa3cac8
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2018-11-07 11:18:27 +0100

    Fix #8

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -2,13 +2,20 @@ require 'rails_helper'
 
 describe "Index author page", type: :feature do
 
+  before :all do
+    @author = FactoryBot.create(:author)
+  end
   it "should render withour error" do
     visit authors_path
   end
 
-  it "should have link to create a new author" do
+  it "should have a link to the author's page" do
     visit authors_path
+    expect(page).to have_link 'Show', href: author_path(@author)
+  end
 
+  it "should have a link to create a new author" do
+    visit authors_path
     expect(page).to have_link 'New Author', href: new_author_path
   end
 end
