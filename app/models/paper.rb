b70734ed8da48b0d02cb0f333d04a7ca73e24e23
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2018-11-07 14:09:29 +0100

    Fix #18

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,6 +1,6 @@
 class Paper < ApplicationRecord
 
   validates :title, presence: true
-  validates :year, presence: true
+  validates :year, presence: true, numericality: true
   validates :venue, presence: true
 end
