15fe8b713a516e597fb5b44b2ffd8adbf2f2631d
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2018-11-07 14:18:08 +0100

    Fix #19

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 describe Paper, type: :model do
 
+  before :all do
+    @paper = FactoryBot.create(:paper)
+  end
+
   it "should be invalid if left empty" do
     paper = Paper.new
     expect(paper).to_not be_valid
