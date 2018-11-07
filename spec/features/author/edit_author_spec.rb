dabc4a6030f8b56024912818533be0a9f1b02893
Adrian Ziegler <adrian.ziegler@student.hpi.de>
2018-11-07 12:26:50 +0100

    Failing test for #13

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -10,4 +10,13 @@ describe "Edit author page", type: :feature do
     visit edit_author_path(@author)
   end
 
+  it "should save the changes" do
+    visit edit_author_path(@author)
+    page.fill_in 'author[last_name]', with: 'Bratan'
+    find('input[type="submit"]').click
+
+    @author.reload
+    expect(@author.last_name).to eq "Bratan"
+  end
+
 end
